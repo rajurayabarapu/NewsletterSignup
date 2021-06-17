3a1f1680eb05d137b6f27118bd91b333005da77a
rajurayabarapu <86067701+rajurayabarapu@users.noreply.github.com>
2021-06-17 23:08:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const request=https.request(url,options,function(response){
     console.log(JSON.parse(data));
   });
 });
-// request.write(jsonData);
+request.write(jsonData);
 request.end();
 
 });
